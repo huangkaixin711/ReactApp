dc607277a3ef648787d1b1b9362d707762025438
黄凯欣 <44826019+huangkaixin711@users.noreply.github.com>
2020-10-17 22:06:31 +0800

    第二次作业
    ReactApp作业提交

---
+++ build/precache-manifest.59ebdab66aecf4151a737a0b48852eac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4dea2fed6946020ed874791a9cd064",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd9c4872ac5f51cecf2b",
+    "url": "/static/css/main.c1990932.chunk.css"
+  },
+  {
+    "revision": "679b6cc50a7af3a9a33c",
+    "url": "/static/js/2.b3794ffe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b3794ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd9c4872ac5f51cecf2b",
+    "url": "/static/js/main.42a54317.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
